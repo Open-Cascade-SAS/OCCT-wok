66835b720513e8f3cf1d1706ac1dcda54cbcc0b7
cas <cas@opencascade.com>
1999-10-22 18:05:40 +0000

    Initial revision

---
+++ src/WOKNT/WOKNT_TimeStat.hxx
@@ -0,0 +1,14 @@
+#ifndef __WOKNT_FILETIME_HXX
+# define __WOKNT_FILETIME_HXX
+
+# ifndef STRICT
+#  define STRICT
+# endif  // STRICT
+
+# ifndef _INC_TIME
+#  include <time.h>
+# endif  // _INC_TIME
+
+typedef time_t WOKNT_TimeStat;
+
+#endif  // __WOKNT_FILETIME_HXX
