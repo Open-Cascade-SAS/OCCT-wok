b51b8f7dc57d350ac38da09b5fc7565a94f8bf45
cas <cas@opencascade.com>
1999-10-22 18:05:40 +0000

    Initial revision

---
+++ src/WOKUtils/WOKUtils_Path_proto.hxx
@@ -0,0 +1,27 @@
+// File:	WOKUtils_Path_proto.hxx
+// Created:	Fri Jan 31 19:37:37 1997
+// Author:	Jean GAUTIER
+//		<jga@cobrax.paris1.matra-dtv.fr>
+
+
+#ifndef WOKUtils_Path_proto_HeaderFile
+#define WOKUtils_Path_proto_HeaderFile
+
+#include <Handle_WOKUtils_Path.hxx>
+
+#ifdef WNT
+
+#include <WOKNT_Path.hxx>
+#define  WOKUtils_Path WOKNT_Path 
+
+#else
+
+#include <WOKUnix_Path.hxx>
+#define  WOKUtils_Path WOKUnix_Path
+
+#endif
+
+#include <WOKUtils_Extension.hxx>
+
+
+#endif
