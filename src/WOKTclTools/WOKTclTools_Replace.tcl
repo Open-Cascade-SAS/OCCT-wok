7487a82148e7808cef9a27be52639b7bd49936ed
cas <cas@opencascade.com>
1999-11-10 18:01:32 +0000

    No comments

--- src/WOKTclTools/WOKTclTools_Replace.tcl
+++ src/WOKTclTools/WOKTclTools_Replace.tcl
@@ -28,7 +28,7 @@ proc WOKTclTools_Replace:Execute { unit args } {
     set tcl_interactive 1
     package require Wokutils
 
-    msgprint -i -c "WOKTclTools_Replace:Execute" "Copying of WOKTclTools includes"
+    msgprint -i -c "WOKTclTools_Replace::Execute" "Copying of WOKTclTools includes"
 
     if { [wokparam -e %Station $unit] != "wnt" } {
 	set copycmd "cp -p "
@@ -53,16 +53,16 @@ proc WOKTclTools_Replace:Execute { unit args } {
 
 	if { [wokparam -e %Station $unit] == "wnt" && $result != "" } {
 	    set result 0
-	}
+	} else { set result 1 }
 	
 	if { ! $result } {
-	    msgprint -i -c "WOKTclTools_Replace:Execute" "Copy $source to $target"
+	    msgprint -i -c "WOKTclTools_Replace::Execute" "Copy $source to $target"
 	    if { [wokparam -e %Station $unit] != "wnt" } {
 		eval exec "chmod u+w $target"
 	    }
 	    eval exec "$copycmd $TheArgs"
 	} else {
-	    msgprint -i -c "WOKTclTools_Replace:Execute" "No change in $source"
+	    msgprint -i -c "WOKTclTools_Replace::Execute" "No change in $source"
 	}
     }
     return 0;
