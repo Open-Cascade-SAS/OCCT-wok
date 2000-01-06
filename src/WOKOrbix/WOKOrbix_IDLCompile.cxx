405ba4147df2b32979fca0bd29ea76b931353fe8
cas <cas@opencascade.com>
2000-01-06 17:56:55 +0000

    No comments

--- src/WOKOrbix/WOKOrbix_IDLCompile.cxx
+++ src/WOKOrbix/WOKOrbix_IDLCompile.cxx
@@ -3,7 +3,11 @@
 // Author:	Jean GAUTIER
 //		<jga@hourax.paris1.matra-dtv.fr>
 
+#ifdef WNT
+#include <io.h>
+#else
 #include <unistd.h>
+#endif
 #include <fstream.h>
 
 #include <TCollection_HAsciiString.hxx>
