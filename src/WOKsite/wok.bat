ce4155c21ed9b579465e640b09357f86507cc29f
cascade <cascade@opencascade.com>
2003-11-19 22:21:34 +0000

    wokcfg and wokcfg.bat were substituted by wokinit and wokinit.bat

--- src/WOKsite/wok.bat
+++ src/WOKsite/wok.bat
@@ -1,10 +1,10 @@
-set TCLHOME=w:/TCL/TCL8.3/tcl8.3.2
-set TCLBIN=%TCLHOME%/win/Release
-set WOKHOME=.
-set PATH=D%TCLBIN%;%WOKHOME%/lib/wnt;%PATH%
-
-rem If Not Defined TCL_RCFILE set TCL_RCFILE=%WOKHOME%\.tclshrc
-
-
+set TCLHOME=%CASROOT%/../3rdparty/win32/tcltk
+set TCLBIN=%TCLHOME%/bin
+set WOKHOME=%CASROOT%/../wok
+set PATH=%TCLBIN%;%WOKHOME%/lib/wnt;%PATH%
+set TCL_RCFILE=%WOKHOME%/site/tclshrc.tcl
+set WOK_ROOTADMDIR=%WOKHOME%/wok_entities
+set HOME=%WOKHOME%/site
+set INIT=%WOKHOME%/lib/wnt
+cd %WOK_ROOTADMDIR%
 %TCLBIN%/tclsh.exe
-
