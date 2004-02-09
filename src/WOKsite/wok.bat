5ea39bef3d5d45cb4782809b7500e8682fc975c8
cascade <cascade@opencascade.com>
2004-02-09 11:22:16 +0000

    5082,5083 Some corrections and improvements in WOK Java extractor.cdl,cxx,h,java,edl need wok modification

--- src/WOKsite/wok.bat
+++ src/WOKsite/wok.bat
@@ -2,9 +2,9 @@ set TCLHOME=%CASROOT%/../3rdparty/win32/tcltk
 set TCLBIN=%TCLHOME%/bin
 set WOKHOME=%CASROOT%/../wok
 set PATH=%TCLBIN%;%WOKHOME%/lib/wnt;%PATH%
-set TCL_RCFILE=%WOKHOME%/site/tclshrc.tcl
+set TCL_RCFILE=%HOME%/tclshrc.tcl
 set WOK_ROOTADMDIR=%WOKHOME%/wok_entities
-set HOME=%WOKHOME%/site
 set INIT=%WOKHOME%/lib/wnt
-cd %WOK_ROOTADMDIR%
+set HOME=%WOKHOME%/site
+cd /d %WOK_ROOTADMDIR%
 %TCLBIN%/tclsh.exe
