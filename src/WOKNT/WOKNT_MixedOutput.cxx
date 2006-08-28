bc1a5188ff081e668d7d825a5effc8ccd229cc33
cascade <cascade@opencascade.com>
2006-08-28 12:12:02 +0000

    compilation error correction cdl, cxx

--- src/WOKNT/WOKNT_MixedOutput.cxx
+++ src/WOKNT/WOKNT_MixedOutput.cxx
@@ -14,7 +14,7 @@ void                                      __fastcall _WOKNT_clear_pipe ( HANDLE 
 DWORD                                     __fastcall _WOKNT_nb_to_read ( HANDLE );
 Handle( TColStd_HSequenceOfHAsciiString ) __fastcall _WOKNT_read_pipe ( OSD_File*, HANDLE );
 void                                      __fastcall _WOKNT_create_pipe (
-                                                      Standard_Size*, Standard_Integer*
+                                                      Standard_Address*, Standard_Address*
                                                      );
 
 WOKNT_MixedOutput :: WOKNT_MixedOutput () {
@@ -28,9 +28,9 @@ void WOKNT_MixedOutput :: Cleanup () {
 
 }  // end WOKNT_MixedOutput :: Cleanup
 
-Standard_Integer WOKNT_MixedOutput :: OpenStdOut () {
+Standard_Address WOKNT_MixedOutput :: OpenStdOut () {
 
- _WOKNT_create_pipe ( &myFileChannel, &myOutHandle );
+ _WOKNT_create_pipe ((Standard_Address *)&myFileChannel, &myOutHandle);
  myIO |= ( FLAG_PIPE | FLAG_READ_PIPE );
 
  return myOutHandle;
@@ -42,13 +42,13 @@ void WOKNT_MixedOutput :: CloseStdOut () {
  if (  ( HANDLE )myOutHandle != INVALID_HANDLE_VALUE  ) {
  
   CloseHandle (  ( HANDLE )myOutHandle  );
-  myOutHandle = ( Standard_Integer )INVALID_HANDLE_VALUE;
+  myOutHandle = (Standard_Address) INVALID_HANDLE_VALUE;
 
  }  // end if
 
 }  // end WOKNT_MixedOutput :: CloseStdOut
 
-Standard_Integer WOKNT_MixedOutput :: OpenStdErr () {
+Standard_Address WOKNT_MixedOutput :: OpenStdErr () {
 
  return (  ( HANDLE )myOutHandle == INVALID_HANDLE_VALUE  ) ?
          OpenStdOut () : myOutHandle;
@@ -171,10 +171,15 @@ Handle( TColStd_HSequenceOfHAsciiString ) __fastcall _WOKNT_read_pipe (
 
 }  // end _WOKNT_read_pipe
 
-void __fastcall _WOKNT_create_pipe (
-                 Standard_Size* readPipe, Standard_Integer* writePipe
-                ) {
-                
+//=======================================================================
+//function : _WOKNT_create_pipe
+//purpose  : 
+//=======================================================================
+
+void __fastcall _WOKNT_create_pipe (Standard_Address* readPipe,
+                                    Standard_Address* writePipe)
+{
+               
  SECURITY_ATTRIBUTES sa;
 
  sa.nLength              = sizeof ( SECURITY_DESCRIPTOR );  // structure size
@@ -187,7 +192,7 @@ void __fastcall _WOKNT_create_pipe (
          &sa,                   // protection/inheritance
          4096                   // buffer size
         )
- ) *writePipe = ( Standard_Integer )INVALID_HANDLE_VALUE;
+ ) *writePipe = ( Standard_Address )INVALID_HANDLE_VALUE;
                 
 }  // end _WOKNT_create_pipe
 #endif
--- src/WOKNT/WOKNT_OutErrOutput.cxx
+++ src/WOKNT/WOKNT_OutErrOutput.cxx
@@ -11,7 +11,7 @@ void                                      __fastcall _WOKNT_clear_pipe ( HANDLE 
 DWORD                                     __fastcall _WOKNT_nb_to_read ( HANDLE );
 Handle( TColStd_HSequenceOfHAsciiString ) __fastcall _WOKNT_read_pipe ( OSD_File*, HANDLE );
 void                                      __fastcall _WOKNT_create_pipe (
-                                                      Standard_Integer*, Standard_Integer*
+                                                      Standard_Address*, Standard_Address*
                                                      );
 
 WOKNT_OutErrOutput :: WOKNT_OutErrOutput () {
@@ -26,7 +26,7 @@ void WOKNT_OutErrOutput :: Cleanup () {
  if (  ( HANDLE )myErrHandleR != INVALID_HANDLE_VALUE  ) {
  
   CloseHandle (  ( HANDLE )myErrHandleR  );
-  myErrHandleR = ( Standard_Integer )INVALID_HANDLE_VALUE;
+  myErrHandleR = (Standard_Address) INVALID_HANDLE_VALUE;
 
  }  // end if
 
@@ -34,7 +34,7 @@ void WOKNT_OutErrOutput :: Cleanup () {
 
 }  // end WOKNT_OutErrOutput :: Cleanup
 
-Standard_Integer WOKNT_OutErrOutput :: OpenStdErr () {
+Standard_Address WOKNT_OutErrOutput :: OpenStdErr () {
 
  _WOKNT_create_pipe ( &myErrHandleR, &myErrHandleW );
  myIO |= ( FLAG_PIPE | FLAG_READ_PIPE );
@@ -48,7 +48,7 @@ void WOKNT_OutErrOutput :: CloseStdErr () {
  if (  ( HANDLE )myErrHandleW != INVALID_HANDLE_VALUE  ) {
  
   CloseHandle (  ( HANDLE )myErrHandleW  );
-  myErrHandleW = ( Standard_Integer )INVALID_HANDLE_VALUE;
+  myErrHandleW = ( Standard_Address )INVALID_HANDLE_VALUE;
 
  }  // end if
 
@@ -65,12 +65,11 @@ void WOKNT_OutErrOutput :: Clear () {
 Handle( TColStd_HSequenceOfHAsciiString ) WOKNT_OutErrOutput :: Errors () {
 
  Handle( TColStd_HSequenceOfHAsciiString ) retVal;
- Standard_Integer                          handle;
+ const Standard_Size                       handle (myFileChannel);
 
- handle        = myFileChannel;
- myFileChannel = myErrHandleR;
+ myFileChannel = (Standard_Size) myErrHandleR;
 
- retVal = _WOKNT_read_pipe (  this, ( HANDLE )myFileChannel  );
+ retVal = _WOKNT_read_pipe (  this, ( HANDLE )myErrHandleR  );
 
  myFileChannel = handle;
 
