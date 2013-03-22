638d0766791a635f6562e4df561240e3320d350b
ibs <ibs@opencascade.com>
2013-03-22 14:06:50 +0400

    0023848: cmake configuration process doesn't correctly search freetype includes in some cases
    also path to env.sh corrected in draw.sh
    Light update

--- src/WOKTclLib/templates/draw.sh
+++ src/WOKTclLib/templates/draw.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source ./env.sh "$1"
+aScriptPath=${BASH_SOURCE%/*}; if [ -d "${aScriptPath}" ]; then cd "$aScriptPath"; fi; aScriptPath="$PWD";
+
+source "${aScriptPath}/env.sh" "$1"
 
 echo 'Hint: use "pload ALL" command to load standard commands'
 DRAWEXE
